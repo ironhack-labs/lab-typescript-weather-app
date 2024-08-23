baa825a86d3890bf7c4c3a7a0dbc7eab58c967d9
soulaimanEltoumi <soulaimaneltoumi@gmail.com>
2024-08-23 13:28:03 +0200

    lab solved

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,36 @@
-// src/main.ts
+import {
+  getLocation,
+  getCurrentWeather,
+  displayLocation,
+  displayWeatherData,
+} from "./utils.ts";
+
+const form = document.getElementById("weather-form") as HTMLFormElement;
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const locationInput = document.getElementById("location") as HTMLInputElement;
+  const locationName = locationInput.value;
+  locationInput.value = "";
+
+  getLocation(locationName)
+    .then((response) => {
+      if (response.results) {
+        const location = response.results[0];
+
+        displayLocation(location);
+
+        return getCurrentWeather(location);
+      } else {
+        throw new Error("Location not found");
+      }
+    })
+    .then((weatherData) => {
+      displayWeatherData(weatherData);
+    })
+    .catch((error) => {
+      console.log("Error getting weather data");
+      console.log(error);
+    });
+});

7e42fc3d9e3226d284d472fc5317ec4b50e63c7a
Skyteks <Schattenpalkia@googlemail.com>
2024-11-27 12:26:52 +0100

    done

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,30 @@
 // src/main.ts
+
+import { displayLocation, displayWeatherData, getCurrentWeather, getLocation } from "./utils";
+
+// src/main.ts
+
+const weatherForm = document.getElementById("weather-form") as HTMLFormElement;
+
+weatherForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const locationInput = document.getElementById("location") as HTMLInputElement;
+    const locationName = locationInput.value;
+    locationInput.value = "";
+
+    getLocation(locationName)
+        .then((response) => {
+            if (!response.results) {
+                throw new Error("Location not found");
+            }
+            const location = response.results[0];
+            displayLocation(location);
+            return getCurrentWeather(location);
+        })
+        .then((weatherData) => {
+            displayWeatherData(weatherData);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+});
--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,47 @@
 // src/utils.ts
 
-import axios from 'axios';
-import { LocationResponse, Location } from "./types";
-
-
+import axios from "axios";
+import { LocationResponse, Location, WeatherResponse } from "./types";
 
 export function getLocation(locationName: string): Promise<LocationResponse> {
     const url = `https://geocoding-api.open-meteo.com/v1/search?name=${locationName}&count=1`;
-    return axios.get(url).then((response) => response.data);
+    console.log("Request:", url);
+    return axios.get(url).then((response) => {
+        console.log("Response:", response.data);
+        return response.data;
+    });
 }
 
+export function getCurrentWeather(locationDetails: Location): Promise<WeatherResponse> {
+    const url = `meteo.com/v1/forecast?latitude=${locationDetails.latitude}&longitude=${locationDetails.longitude}&current_weather=true&models=icon_global`;
+    console.log("Request:", url);
+    return axios.get(url).then((response) => {
+        console.log("Response:", response.data);
+        return response.data;
+    });
+}
 
+export function displayLocation(locationDetails: Location) {
+    const locationElement = document.getElementById("location-name") as HTMLElement;
+    locationElement.innerText = "" + locationDetails.name;
 
+    const countryElement = document.getElementById("country") as HTMLElement;
+    countryElement.innerText = "(" + locationDetails.country + ")";
+}
+
+export function displayWeatherData(obj: WeatherResponse) {
+    const temperatureElement = document.getElementById("temperature") as HTMLElement;
+    const temperature = obj.current_weather.temperature;
+    const temperatureUnits = obj.current_weather_units.temperature;
+    temperatureElement.innerText = `Temperature: ${temperature} ${temperatureUnits}`;
+
+    const windspeedElement = document.getElementById("windspeed") as HTMLElement;
+    const windspeed = obj.current_weather.windspeed;
+    const windspeedUnits = obj.current_weather_units.windspeed;
+    windspeedElement.innerText = `Wind Speed: ${windspeed} ${windspeedUnits}`;
+
+    const winddirectionElement = document.getElementById("winddirection") as HTMLElement;
+    const winddirection = obj.current_weather.winddirection;
+    const winddirectionUnits = obj.current_weather_units.winddirection;
+    winddirectionElement.innerText = `Wind Direction: ${winddirection} ${winddirectionUnits}`;
+}

2c3bc20e63055544325f58446adf5463ff334ffd
monica am <monica.andres.consulting@gmail.com>
2024-11-30 22:56:16 +0100

    done

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,7 @@
 
 import axios from 'axios';
 import { LocationResponse, Location } from "./types";
-
+import { LocationResponse, Location, WeatherResponse } from "./types.ts";
 
 
 export function getLocation(locationName: string): Promise<LocationResponse> {
@@ -10,5 +10,38 @@ export function getLocation(locationName: string): Promise<LocationResponse> {
     return axios.get(url).then((response) => response.data);
 }
 
+export function getCurrentWeather(locationDetails: Location): Promise<WeatherResponse> {
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${locationDetails.latitude}&longitude=${locationDetails.longitude}&current_weather=true&models=icon_global`;
+    return axios.get(url).then((response) => response.data);
+}
+
+export function displayLocation(locationDetails: Location) {
+    // display location name
+    const locationNameElm = document.getElementById('location-name') as HTMLElement;
+    locationNameElm.innerText = "" + locationDetails.name;
+
+    // display country
+    const countryElm = document.getElementById('country') as HTMLElement;
+    countryElm.innerText = "(" + locationDetails.country + ")";
+}
 
 
+export function displayWeatherData(obj: WeatherResponse) {
+    // display temperature  
+    const temperatureElm = document.getElementById('temperature') as HTMLElement;
+    const temperature = obj.current_weather.temperature;
+    const temperatureUnits = obj.current_weather_units.temperature;
+    temperatureElm.innerText = `Temperature: ${temperature} ${temperatureUnits}`;
+    
+    // display wind speed
+    const windspeedElm = document.getElementById('windspeed') as HTMLElement;
+    const windspeed = obj.current_weather.windspeed;
+    const windspeedUnits = obj.current_weather_units.windspeed;
+    windspeedElm.innerText = `Wind Speed: ${windspeed} ${windspeedUnits}`;
+
+    // display wind direction
+    const winddirectionElm = document.getElementById('winddirection') as HTMLElement;
+    const winddirection = obj.current_weather.winddirection;
+    const winddirectionUnits = obj.current_weather_units.winddirection;
+    winddirectionElm.innerText = `Wind Direction: ${winddirection} ${winddirectionUnits}`;
+}

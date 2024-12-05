8ea0e8eee73b866e58d5e4a0dd1fbb21821dbd6e
eslahkonha.mona@gmail.com <eslahkonha.mona@gmail.com>
2024-12-05 17:19:06 +0100

    lab completed

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,40 @@
-// src/main.ts
+import {
+  getLocation,
+  getCurrentWeather,
+  displayLocation,
+  displayWeatherData,
+  updateBackground
+} from './utils.ts'
+
+const form = document.getElementById('weather-form') as HTMLFormElement
+
+form.addEventListener('submit', event => {
+  event.preventDefault()
+
+  const locationInput = document.getElementById('location') as HTMLInputElement
+  const locationName = locationInput.value
+  locationInput.value = ''
+
+  getLocation(locationName)
+    .then(response => {
+      if (response.results) {
+        const location = response.results[0]
+        displayLocation(location)
+        return getCurrentWeather(location)
+      } else {
+        throw new Error('Location not found')
+      }
+    })
+    .then(weatherData => {
+      displayWeatherData(weatherData)
+
+      // Update the background
+      updateBackground(
+        weatherData.current_weather.weathercode,
+        weatherData.current_weather.is_day
+      )
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error)
+    })
+})
--- src/types.ts
+++ src/types.ts
@@ -1,30 +1,59 @@
 // src/types.ts
 
 export type Location = {
-    id: number;
-    name: string;
-    latitude: number;
-    longitude: number;
-    elevation: number;
-    feature_code: string;
-    country_code: string;
-    timezone: string;
-    population: number;
-    postcodes: string[];
-    country_id: number;
-    country: string;
-    admin1?: string;
-    admin2?: string;
-    admin3?: string;
-    admin4?: string;
-    admin1_id?: number;
-    admin2_id?: number;
-    admin3_id?: number;
-    admin4_id?: number;
+  id: number
+  name: string
+  latitude: number
+  longitude: number
+  elevation: number
+  feature_code: string
+  country_code: string
+  timezone: string
+  population: number
+  postcodes: string[]
+  country_id: number
+  country: string
+  admin1?: string
+  admin2?: string
+  admin3?: string
+  admin4?: string
+  admin1_id?: number
+  admin2_id?: number
+  admin3_id?: number
+  admin4_id?: number
 }
 
 export type LocationResponse = {
-    results?: Location[];
-    generationtime_ms: number;
+  results?: Location[]
+  generationtime_ms: number
 }
 
+export type WeatherCurrent = {
+  time: string
+  temperature: number
+  windspeed: number
+  winddirection: number
+  is_day: boolean
+  weathercode: number
+}
+
+export type CurrentWeatherUnits = {
+  time: string
+  temperature: string
+  windspeed: string
+  winddirection: string
+  is_day: string
+  weathercode: string
+}
+
+export type WeatherResponse = {
+  latitude: number
+  longitude: number
+  generationTimeMs: number
+  utcOffsetSeconds: number
+  timezone: string
+  timezoneAbbreviation: string
+  elevation: number
+  currentWeatherUnits: CurrentWeatherUnits
+  currentWeather: WeatherCurrent
+}

059dda77842f207609bf50fc98dcad62ff4e8334
Moaz Mubaydin <mmubaydin@gmail.com>
2024-12-05 21:43:15 +0100

    implemented function to handle submit

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,26 @@
 // src/main.ts
+import {  displayLocation, getLocation, getCurrentWeather, displayWeatherData } from "./utils";
+const submit = document.getElementById("weather-form") as HTMLElement;
+
+submit.addEventListener("submit",async (e)=>{
+    e.preventDefault()
+    const input = document.getElementById("location") as HTMLInputElement
+
+   try{
+        const getLocationResponse = await getLocation(input.value);
+        if(getLocationResponse.results){
+              displayLocation(getLocationResponse?.results[0])
+             const currWeather =  await getCurrentWeather(getLocationResponse.results[0])
+             if(currWeather){
+                displayWeatherData(currWeather)
+
+             }
+        }
+       
+
+    }catch(error){
+        console.log(error)
+    }
+
+})
+

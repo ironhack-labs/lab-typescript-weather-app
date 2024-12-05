431a1434d715edc214696747b60006f23a16ed51
nathidaum <daumnathi@gmail.com>
2024-12-05 16:20:03 +0100

    finish lab

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,38 @@
-// src/main.ts
+import {
+  getLocation,
+  getCurrentWeather,
+  displayLocation,
+  displayWeatherData,
+  updateBackground,
+} from "./utils.ts";
+
+const weatherform = document.getElementById("weather-form") as HTMLFormElement;
+
+weatherform.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const locationInput = document.getElementById("location") as HTMLInputElement;
+  const locationName = locationInput.value;
+  locationInput.value = "";
+
+  getLocation(locationName)
+    .then((response) => {
+      if (response.results) {
+        const location = response.results[0];
+        displayLocation(location);
+        return getCurrentWeather(location);
+      } else {
+        throw new Error("Location not found");
+      }
+    })
+    .then((weatherData) => {
+      displayWeatherData(weatherData);
+      updateBackground(
+        weatherData.current_weather.weathercode,
+        weatherData.current_weather.is_day
+      );
+    })
+    .catch((err) => {
+      console.log("Error getting weather data", err);
+    });
+});
--- src/utils.ts
+++ src/utils.ts
@@ -1,14 +1,82 @@
 // src/utils.ts
 
-import axios from 'axios';
+import axios from "axios";
 import { LocationResponse, Location } from "./types";
-
-
+import { WeatherResponse } from "./main";
 
 export function getLocation(locationName: string): Promise<LocationResponse> {
-    const url = `https://geocoding-api.open-meteo.com/v1/search?name=${locationName}&count=1`;
-    return axios.get(url).then((response) => response.data);
+  const url = `https://geocoding-api.open-meteo.com/v1/search?name=${locationName}&count=1`;
+  return axios.get(url).then((response) => response.data);
 }
 
+export function getCurrentWeather(
+  locationDetails: Location
+): Promise<WeatherResponse> {
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${locationDetails.latitude}&longitude=${locationDetails.longitude}&current_weather=true&models=icon_global`;
+  return axios.get(url).then((response) => response.data);
+}
 
+export function displayLocation(locationDetails: Location) {
+    const locationName = document.getElementById("location-name") as HTMLElement;
+    const country = document.getElementById("country") as HTMLElement;
 
+    locationName.innerText = `${locationDetails.name}`
+    country.innerText = `${locationDetails.country}`
+}
+
+export function displayWeatherData(obj: WeatherResponse): void {
+    const temperature = document.getElementById("temperature") as HTMLElement;
+    const windspeed = document.getElementById("windspeed") as HTMLElement;
+    const winddirection = document.getElementById("winddirection") as HTMLElement;
+
+    temperature.innerText = `Temperature: ${obj.current_weather.temperature} ${obj.current_weather_units.temperature}`
+    windspeed.innerText = `Wind speed: ${obj.current_weather.windspeed} ${obj.current_weather_units.windspeed}`
+    winddirection.innerText = `Wind direction: ${obj.current_weather.winddirection} ${obj.current_weather_units.winddirection}`
+}
+
+export function updateBackground(weatherCode: number, isDay: number): void {
+    if ((weatherCode === 0 || weatherCode === 1) && isDay === 0) {
+        document.body.className = "sunny-night";
+        return;
+    } 
+    if ((weatherCode === 0 || weatherCode === 1) && isDay === 1) {
+        document.body.className = "sunny";
+        return;
+    } 
+    if (weatherCode === 2 && isDay === 0) {
+        document.body.className = "partly-cloudy-night";
+        return;
+    } 
+    if (weatherCode === 2 && isDay === 1) {
+        document.body.className = "partly-cloudy";
+        return;
+    } 
+    if (weatherCode === 3) {
+        document.body.className = "cloudy";
+        return;
+    } 
+    if (weatherCode === 4) {
+        document.body.className = "foggy";
+        return;
+    } 
+    if (weatherCode === 5) {
+        document.body.className = "drizzle";
+        return;
+    } 
+    if (weatherCode === 6) {
+        document.body.className = "rain";
+        return;
+    } 
+    if (weatherCode === 7) {
+        document.body.className = "snow";
+        return;
+    } 
+    if (weatherCode === 8) {
+        document.body.className = "showers";
+        return;
+    } 
+    if (weatherCode === 9) {
+        document.body.className = "thunderstorm";
+        return;
+    } 
+}
